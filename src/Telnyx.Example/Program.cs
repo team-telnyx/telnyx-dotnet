9f9b2ccdb0544f06b355a6f52c9b5828452cf09e
eoghan <eoghan@telnyx.com>
2020-02-11 16:07:23 -0600

    updated config setup for example app

--- src/Telnyx.Example/Program.cs
+++ src/Telnyx.Example/Program.cs
@@ -8,8 +8,7 @@ namespace Telnyx.Example
         {
             Console.WriteLine("Starting Examples...");
 
-            TelnyxConfiguration.SetApiBase($"http://localhost:12111/v2");
-            TelnyxConfiguration.SetFilesBase($"http://localhost:12111/v2");
+            TelnyxConfiguration.SetApiBase($"https://api.telnyx.com/v2");
             TelnyxConfiguration.SetApiKey("YOUR_API_KEY");
 
             Console.WriteLine("MessagesExample...");
