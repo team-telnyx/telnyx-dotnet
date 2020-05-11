3d736167ee3897ed592e3ee451f59cadd56355c4
David Maman <david.maman@hotmail.com>
2020-05-11 16:13:26 -0500

    uncomment examples

--- src/Telnyx.Example/Program.cs
+++ src/Telnyx.Example/Program.cs
@@ -18,59 +18,59 @@ namespace Telnyx.Example
             TelnyxConfiguration.SetApiBase("https://api.telnyx.com/v2");
             TelnyxConfiguration.SetApiKey(config["TelnyxApiKey"]);
 
-            //Console.WriteLine("MessagesExample...");
-            //MessagesExample messagesExample = new MessagesExample();
-            //await messagesExample.SendMessage();
-            //messagesExample.RetrieveMessage();
+            Console.WriteLine("MessagesExample...");
+            MessagesExample messagesExample = new MessagesExample();
+            await messagesExample.SendMessage();
+            messagesExample.RetrieveMessage();
 
-            //Console.WriteLine("MessagingProfilesExample...");
-            //MessagingProfilesExample messagingProfilesExample = new MessagingProfilesExample();
-            //messagingProfilesExample.List();
-            //messagingProfilesExample.ListAllMessagingProfilePhoneNumbers();
-            //messagingProfilesExample.Create();
-            //messagingProfilesExample.Update();
-            //messagingProfilesExample.Delete();
+            Console.WriteLine("MessagingProfilesExample...");
+            MessagingProfilesExample messagingProfilesExample = new MessagingProfilesExample();
+            messagingProfilesExample.List();
+            messagingProfilesExample.ListAllMessagingProfilePhoneNumbers();
+            messagingProfilesExample.Create();
+            messagingProfilesExample.Update();
+            messagingProfilesExample.Delete();
 
-            //Console.WriteLine("NumberOrderExample...");
-            //NumberOrderExample numberOrderExample = new NumberOrderExample();
-            //numberOrderExample.ListNumberOrder();
-            //numberOrderExample.CreateNumberOrder();
+            Console.WriteLine("NumberOrderExample...");
+            NumberOrderExample numberOrderExample = new NumberOrderExample();
+            numberOrderExample.ListNumberOrder();
+            numberOrderExample.CreateNumberOrder();
 
-            //Console.WriteLine("NumberReservationExample...");
-            //NumberReservationExample numberReservationExample = new NumberReservationExample();
-            //numberReservationExample.CreateNumberReservation();
-            //numberReservationExample.ListNumberReservation();
+            Console.WriteLine("NumberReservationExample...");
+            NumberReservationExample numberReservationExample = new NumberReservationExample();
+            numberReservationExample.CreateNumberReservation();
+            numberReservationExample.ListNumberReservation();
 
-            //Console.WriteLine("NumberSearchExample...");
-            //NumberSearchExample numberSearchExample = new NumberSearchExample();
-            //numberSearchExample.ListNumberService();
+            Console.WriteLine("NumberSearchExample...");
+            NumberSearchExample numberSearchExample = new NumberSearchExample();
+            numberSearchExample.ListNumberService();
 
-            //Console.WriteLine("PhoneNumbersExample...");
-            //PhoneNumbersExample phoneNumbersExample = new PhoneNumbersExample();
-            //phoneNumbersExample.GetPhoneNumber();
-            //phoneNumbersExample.ListNumberOrder();
-            //phoneNumbersExample.UpdatePhoneNumber();
+            Console.WriteLine("PhoneNumbersExample...");
+            PhoneNumbersExample phoneNumbersExample = new PhoneNumbersExample();
+            phoneNumbersExample.GetPhoneNumber();
+            phoneNumbersExample.ListNumberOrder();
+            phoneNumbersExample.UpdatePhoneNumber();
 
-            //Console.WriteLine("CallControlExample...");
-            //CallControlExample callControlExample = new CallControlExample();
-            //callControlExample.Dial();
+            Console.WriteLine("CallControlExample...");
+            CallControlExample callControlExample = new CallControlExample();
+            callControlExample.Dial();
 
-            //callControlExample.SetCallControlId("v2:ZKoCq-ggJPRhO3H9KxQa5zVQphVX8Hx9d_FRl2b-W2dQfVUHfhe13g");
+            callControlExample.SetCallControlId("v2:ZKoCq-ggJPRhO3H9KxQa5zVQphVX8Hx9d_FRl2b-W2dQfVUHfhe13g");
 
-            //callControlExample.Speak();
-            //callControlExample.Bridge();
-            //callControlExample.ForkingStart();
-            //callControlExample.ForkingStop();
-            //callControlExample.GatherUsingAudio();
-            //callControlExample.GatherUsingSpeak();
-            //callControlExample.Hangup();
-            //callControlExample.PlaybackStart();
-            //callControlExample.PlaybackStop();
-            //callControlExample.RecordStart();
-            //callControlExample.RecordStop();
-            //callControlExample.Reject();
-            //callControlExample.SendDTMF();
-            //callControlExample.Transfer();
+            callControlExample.Speak();
+            callControlExample.Bridge();
+            callControlExample.ForkingStart();
+            callControlExample.ForkingStop();
+            callControlExample.GatherUsingAudio();
+            callControlExample.GatherUsingSpeak();
+            callControlExample.Hangup();
+            callControlExample.PlaybackStart();
+            callControlExample.PlaybackStop();
+            callControlExample.RecordStart();
+            callControlExample.RecordStop();
+            callControlExample.Reject();
+            callControlExample.SendDTMF();
+            callControlExample.Transfer();
 
             Console.WriteLine("NumberConfigurationExample..");
             NumberConfigurationExample numConfigExample = new NumberConfigurationExample();
