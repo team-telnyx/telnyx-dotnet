8cab678e9b4aad5d548cc0365b157510469beffb
rajjy1 <rkarki2009@gmail.com>
2020-07-11 11:28:41 -0700

    Adding sim cardservice and entity

---
+++ src/Telnyx.net/Entities/Wireless/SimCards/SimCardRecord.cs
@@ -0,0 +1,16 @@
+﻿namespace Telnyx.net.Entities.Wireless
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Phone number.
+    /// </summary>
+    public class SimCardRecord: TelnyxEntity, IHasId
+    {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
---
+++ src/Telnyx.net/Services/Wireless/SimCards/SimCardsService.cs
@@ -0,0 +1,21 @@
+﻿namespace Telnyx.net.Services.Wireless.SimCards
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Telnyx.net.Entities.Wireless;
+
+    public class SimCardsService : Service<SimCardRecord>, IRetrievable<SimCardRecord>
+    {
+        public override string BasePath => "/sim_cards";
+
+        public SimCardRecord Get(string id, RequestOptions requestOptions = null)
+        {
+            return this.GetEntity(id, requestOptions);
+        }
+
+        public async Task<SimCardRecord> GetAsync(string id, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
+        {
+            return await this.GetEntityAsync(id, requestOptions, cancellationToken);
+        }
+    }
+}
