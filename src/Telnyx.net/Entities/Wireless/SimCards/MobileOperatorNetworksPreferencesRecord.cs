d74e1e195d7427c873bba5e8953665824fc99c1b
rajjy1 <rkarki2009@gmail.com>
2020-08-02 05:30:11 -0700

    BulkUpdate Interface added. BulkUpdate services added. Put Request handling added. API for Bulk Update Network Preferences. stylecop json for Test project

---
+++ src/Telnyx.net/Entities/Wireless/SimCards/MobileOperatorNetworksPreferencesRecord.cs
@@ -0,0 +1,46 @@
+﻿namespace Telnyx.net.Entities.Wireless.SimCards
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using Telnyx.net.Entities.Wireless.OTAUpdates;
+
+    public class MobileOperatorNetworksPreferencesRecord : TelnyxEntity, IHasId
+    {
+        /// <summary>
+        /// Gets or Sets MobileOperatorNetworksPreferences
+        /// </summary>
+        [JsonProperty("mobile_operator_networks_preferences")]
+        public IList<MobileOperatorNetworksPreferences> MobileOperatorNetworksPreferences { get; set; }
+
+        /// <summary>
+        /// Gets or sets OTA Updates.
+        /// </summary>
+        [JsonProperty("ota_updates")]
+        public IList<OTAUpdatesRecord> OTAUpdate { get; set; }
+
+        /// <summary>
+        /// Gets ts RecordType.
+        /// </summary>
+        [JsonProperty("record_type")]
+        public string RecordType { get; private set; }
+
+        /// <summary>
+        /// The indentification UUID of the related SIM card resource.
+        /// </summary>
+        [JsonProperty("sim_card_id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// ISO 8601 formatted date-time indicating when the resource was created.
+        /// </summary>
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; private set; }
+
+        /// <summary>
+        /// ISO 8601 formatted date-time indicating when the resource was updated.
+        /// </summary>
+        [JsonProperty("updated_at")]
+        public string UpdatedAt { get; private set; }
+    }
+}
---
+++ src/TelnyxTests/Services/Wireless/SimCards/SimCardBulkNetworkPreferenceServiceTest.cs
@@ -0,0 +1,57 @@
+﻿namespace TelnyxTests.Services.Wireless.SimCards
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Telnyx.net.Entities.Wireless.OTAUpdates;
+    using Telnyx.net.Entities.Wireless.SimCards;
+    using Telnyx.net.Services.Wireless.SimCards;
+    using Xunit;
+    using Telnyx.net.Entities;
+
+    public class SimCardBulkNetworkPreferenceServiceTest : BaseTelnyxTest
+    {
+        private readonly SimCardBulkNetworkPreferenceService service;
+        private readonly SimCardBulkNetworkPreferenceUpdateOptions updateOptions;
+
+        private const string parentId = "6a09cdc3-8948-47f0-aa62-74ac943d6c58";
+
+        public SimCardBulkNetworkPreferenceServiceTest(MockHttpClientFixture mockHttpClientFixture)
+            : base(mockHttpClientFixture)
+        {
+            this.service = new SimCardBulkNetworkPreferenceService();
+            this.updateOptions = new SimCardBulkNetworkPreferenceUpdateOptions
+            {
+                SimCardIds = new string[]
+                {
+                    "6b14e151-8493-4fa1-8664-1cc4e6d14158",
+                    "6b14e151-8493-4fa1-8664-1cc4e6d14158",
+                },
+                MobileOperatorNetworksPreferences = new List<MobileOperatorNetworksPreferences>()
+                {
+                    new MobileOperatorNetworksPreferences()
+                    {
+                        MobileOperatorNetworkId = new Guid("6a09cdc3-8948-47f0-aa62-74ac943d6c58"),
+                        Priority = 0,
+                    },
+                },
+            };
+        }
+
+        [Fact]
+        public void Update()
+        {
+            var result = this.service.Update(this.updateOptions);
+            Assert.NotNull(result);
+            Assert.Equal(typeof(TelnyxCollection<MobileOperatorNetworksPreferencesRecord>), result.GetType());
+        }
+
+        [Fact]
+        public async Task UpdateAsync()
+        {
+            var result = await this.service.UpdateAsync(this.updateOptions);
+            Assert.NotNull(result);
+            Assert.Equal(typeof(TelnyxCollection<MobileOperatorNetworksPreferencesRecord>), result.GetType());
+        }
+    }
+}
