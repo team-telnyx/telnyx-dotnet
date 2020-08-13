ba236d71abe4fcb93716261e2473f98f3af59825
Dave Maman <david.maman@hotmail.com>
2020-08-13 17:20:02 -0500

    Fix/add url shortner settings (#21)
    * add URL Shortner Settings to messaging profile
    
    * update tests
    
    * Revert some merge errors
    
    * more bad merge fixes
    
    * more bad merge fixes
    
    * add tests and add support for message profile metrics
    
    * cleanup and add examples
    
    * beautify response

---
+++ src/Telnyx.net/Entities/Messaging/Messaging_Profiles/Metrics/MessagingProfileMetricsDetailed.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telnyx.net.Entities.Messaging.Messaging_Profiles.Metrics
+{
+    public class MessagingProfileMetricsDetailed : TelnyxEntity
+    {
+        [JsonProperty("timestamp")]
+        public DateTime? TimeStamp { get; set; }
+
+        [JsonProperty("metrics")]
+        public IEnumerable<MessagingProfileMetrics> Metrics { get; set; }
+    }
+}
