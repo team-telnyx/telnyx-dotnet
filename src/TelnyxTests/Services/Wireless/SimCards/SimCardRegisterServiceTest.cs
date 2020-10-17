301b864358d8dd60c7845752219d0fcd6cbbcfe8
rajjy1 <rkarki2009@gmail.com>
2020-10-17 10:52:50 -0700

    Added test cases for Simcard register

--- src/TelnyxTests/Services/Wireless/SimCards/SimCardRegisterServiceTest.cs
+++ src/TelnyxTests/Services/Wireless/SimCards/SimCardRegisterServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace TelnyxTests.Services.Wireless.SimCards
 {
+    using System.Linq;
     using System.Threading.Tasks;
     using Telnyx.net.Entities;
     using Telnyx.net.Entities.Wireless.SimCards;
@@ -39,6 +40,9 @@
             var result = this.service.Create(this.simCardRegisterOptions);
             Assert.NotNull(result);
             Assert.Equal(typeof(TelnyxCollection<SimCardRecord>), result.GetType());
+            var response = result.Data.FirstOrDefault();
+            Assert.Equal(this.simCardRegisterOptions.SimCardGroupId, response.SimCardGroupId.ToString());
+            Assert.Equal(this.simCardRegisterOptions.Tags.Length, response.Tags.Count());
         }
 
         [Fact]
